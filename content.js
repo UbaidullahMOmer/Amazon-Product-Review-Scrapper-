e1ea6c1ffacb59cb33ba7b36d9e7b7d73f21b08b
Rana Saifullah <121765405+UbaidullahMOmer@users.noreply.github.com>
2024-08-11 00:26:14 +0500

    update

---
+++ content.js
@@ -0,0 +1,36 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "downloadLogos") {
+    downloadCompanyLogos();
+  }
+});
+
+function downloadCompanyLogos() {
+  const companies = [];
+  const companyNamesMainDiv =
+    document.getElementsByClassName("wp-block-heading");
+  const companyImageMainDiv = document.getElementsByClassName("wp-block-image");
+
+  for (let i = 0; i < companyNamesMainDiv.length; i++) {
+    companies[i] = {
+      name: companyNamesMainDiv[i].innerText,
+    };
+  }
+
+  for (let i = 0; i < companyImageMainDiv.length; i++) {
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = companyImageMainDiv[i].innerHTML;
+    const imgElement = tempDiv.querySelector("img");
+    const imgSrc = imgElement ? imgElement.src : "";
+    companies[i] = { ...companies[i], image: imgSrc };
+  }
+
+  companies.forEach((company, index) => {
+    if (company.image) {
+      chrome.runtime.sendMessage({
+        action: "download",
+        url: company.image,
+        filename: `${company.name || `company_${index}`}.png`,
+      });
+    }
+  });
+}
