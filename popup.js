3334347a3947cced649566ced1518330ba957998
Rana Saifullah <121765405+UbaidullahMOmer@users.noreply.github.com>
2024-08-11 00:40:52 +0500

    zxc

--- popup.js
+++ popup.js
@@ -1,5 +1,17 @@
-document.getElementById('downloadButton').addEventListener('click', () => {
-  chrome.tabs.query({active: true, current: true}, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, {action: "downloadLogos"});
+document.getElementById("downloadButton").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (chrome.runtime.lastError) {
+      // If currentWindow fails, try with current_window
+      chrome.tabs.query(
+        { active: true, current_window: true },
+        function (tabs) {
+          if (tabs[0]) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: "downloadLogos" });
+          }
+        }
+      );
+    } else if (tabs[0]) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "downloadLogos" });
+    }
   });
 });
